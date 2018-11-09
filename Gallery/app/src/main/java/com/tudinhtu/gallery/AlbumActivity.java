930ca1fed05b60025948ffbd77d7ea4b787c58ab
DLNHChuong <1612058@student.hcmus.edu.vn>
2018-11-09 22:56:34 +0700

    Hoan thanh chuc nang Back

--- Gallery/app/src/main/java/com/tudinhtu/gallery/AlbumActivity.java
+++ Gallery/app/src/main/java/com/tudinhtu/gallery/AlbumActivity.java
@@ -12,6 +12,8 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -41,7 +43,7 @@ public class AlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
         album_name = intent.getStringExtra("name");
         setTitle(album_name);
@@ -66,7 +68,20 @@ public class AlbumActivity extends AppCompatActivity {
 
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
 
     class LoadAlbumImages extends AsyncTask<String, Void, String> {
         @Override
--- Gallery/app/src/main/java/com/tudinhtu/gallery/GalleryPreview.java
+++ Gallery/app/src/main/java/com/tudinhtu/gallery/GalleryPreview.java
@@ -3,6 +3,9 @@ package com.tudinhtu.gallery;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -15,6 +18,7 @@ import java.io.File;
 public class GalleryPreview extends AppCompatActivity {
 
     ImageView GalleryPreviewImg;
+    Button btnBack;
     String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,16 @@ public class GalleryPreview extends AppCompatActivity {
         Intent intent = getIntent();
         path = intent.getStringExtra("path");
         GalleryPreviewImg = (ImageView) findViewById(R.id.GalleryPreviewImg);
+        btnBack= (Button) findViewById(R.id.Back01);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("toast","1");
+                finish();
+
+            }
+        });
         Glide.with(GalleryPreview.this)
                 .load(new File(path)) // Uri of the picture
                 .into(GalleryPreviewImg);
